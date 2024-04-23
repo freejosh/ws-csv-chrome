4e1620602b5834569a00049926cf383656c091f8
Josh Freeman <freejosh@gmail.com>
2024-04-23 08:22:44 -0300

    First commit

---
+++ background.js
@@ -0,0 +1,12 @@
+/* global chrome */
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    files: ['content-style.css'],
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['any-date-parser.js', 'content-script.js'],
+  });
+});
